4f7fead5ee8bee5f5cefc5574cd419f73a10ef87
jackulin-123 <168457101+jackulin-123@users.noreply.github.com>
2024-05-01 17:31:14 +0800

    Add files via upload
    Week1 Day1

---
+++ week1/day1/SwitchCase.java
@@ -0,0 +1,43 @@
+package week1.day1;
+
+public class SwitchCase {
+	
+		
+		public static void main (String args[]) {
+			
+		 char city = 'M';
+			
+		  switch (city) {
+			
+			case 'M':
+				
+				System.out.println("Chennai city");
+				break;
+				
+			case 'H':
+				
+				System.out.println("Hyderabad city");
+				break;
+				
+			case 'B':
+				
+				System.out.println("Bangalore city");
+				break;
+				
+			case 'Y':
+				
+				System.out.println("Mumbai city");
+				break;
+				
+			case 'D':
+				
+				System.out.println("Delhi city");
+				break;
+			
+			}
+			
+		}
+
+	}
+
+
